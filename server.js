910312bd3ce070d5ddb4f41668b8e5dcd2ff5ca4
gaurav-kr92 <gaurav.kr9232@gmail.com>
2023-07-03 02:18:51 +0530

    Added cart and login paage

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3000 ;
 const path = require('path')
 
 
+app.use(expressLayout)
+app.set('views', path.join( __dirname,'/resources/views'))
+app.set('view engine', 'ejs')
+
+
 // Assets 
 app.use(express.static('public'));
 
@@ -13,9 +18,25 @@ app.get("/", function(req,res){
     res.render('home')
 })
 
-app.use(expressLayout)
-app.set('views', path.join( __dirname,'/resources/views'))
-app.set('view engine', 'ejs')
+app.get("/cart",function(req,res){
+    res.render('customers/cart')
+})
+
+app.get("/login", function(req,res){
+    res.render('auth/login')
+});
+
+app.get("/register",function(req,res){
+    res.render('auth/register')
+})
+
+
+
+
+
+
+
+
 
 
 app.listen(PORT, function(){
