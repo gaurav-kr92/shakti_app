9fa4f745a8dc61bbc492d03ff382828bd81a8517
gaurav-kr92 <gaurav.kr9232@gmail.com>
2023-07-04 01:25:16 +0530

    add to cart

--- server.js
+++ server.js
@@ -4,8 +4,40 @@ const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000 ;
 const path = require('path')
+const mongoose = require('mongoose');
+const session = require('express-session');
+require('dotenv').config();
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongo');
 
 
+const url = 'mongodb://localhost:27017/shakti';
+mongoose.connect(url, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+})
+
+
+
+//Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave:false,
+    store: MongoDbStore.create({ mongoUrl: 'mongodb://localhost:27017/shakti' }),
+    saveUnintialized: false,
+    cookie: { maxAge: 1000*60*60*24}
+
+}))
+
+app.use(flash())
+app.use(express.json())
+
+//Gobal middleware
+app.use((req,res,next)=>{
+    res.locals.session = req.session;
+    next()
+})
 app.use(expressLayout)
 app.set('views', path.join( __dirname,'/resources/views'))
 app.set('view engine', 'ejs')
@@ -14,21 +46,10 @@ app.set('view engine', 'ejs')
 // Assets 
 app.use(express.static('public'));
 
-app.get("/", function(req,res){
-    res.render('home')
-})
 
-app.get("/cart",function(req,res){
-    res.render('customers/cart')
-})
+require('./routes/web')(app)
 
-app.get("/login", function(req,res){
-    res.render('auth/login')
-});
 
-app.get("/register",function(req,res){
-    res.render('auth/register')
-})
 
 
 
